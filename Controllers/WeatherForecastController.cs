52dccbdb979bc663ed2a493a2ba355f2a2f61a46
Sheheer Ali <nssheheerali@gmail.com>
2023-09-01 22:22:00 +0530

    initial commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+using System.Net.Mail;
+using MimeKit;
+using MailKit.Security;
+namespace simplemail.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public string Get()
+    {
+         try
+            {
+                 Console.WriteLine("Sending using .Mailkit! press enter");
+            var email = new MimeMessage();
+
+            email.From.Add(new MailboxAddress("Cubehr App", "nssheheerali@gmail.com"));
+            email.To.Add(new MailboxAddress("SHEHEER", "nssheheerali@gmail.com"));
+
+            email.Subject = "Testing out email sending";
+            email.Body = new TextPart(MimeKit.Text.TextFormat.Html) { 
+                Text = "<b>Hello all the way from the land of C#</b>"
+            };
+
+            using (var smtpc = new MailKit.Net.Smtp.SmtpClient())
+            {
+                smtpc.Connect("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
+
+                // Note: only needed if the SMTP server requires authentication
+                smtpc.Authenticate("nssheheerali@gmail.com", "ngozruvgxmsxizpj");
+
+                smtpc.Send(email);
+                smtpc.Disconnect(true);
+            }
+            Console.WriteLine("sent from Mailkit!");
+             return "succces";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("sent from Mailkit! Failed");
+                return ex.Message;
+            }
+    }
+
+   
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Add Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Your API Name",
+        Version = "v1",
+        Description = "Your API Description"
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API Name v1");
+    });
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
